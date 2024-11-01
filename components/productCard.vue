eccc73430a6c2337402046b781841908c8206796
tamXinChao <dominhtam0175@gmail.com>
2024-11-01 13:59:52 +0700

    Reusable components

---
+++ components/productCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card text-center">
+    <img
+      :src="product.image"
+      alt="Product thumb"
+      class="w-full h-48 object-cover rounded-md mb-4 thumb"
+    />
+    <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
+    <NuxtLink :to="`/products/${product.id}`"
+      ><p class="btn mt-4">View detail</p></NuxtLink
+    >
+  </div>
+</template>
+
+<script setup>
+const { product } = defineProps(["product"]);
+</script>
+
+<style scoped></style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,24 +1,5 @@
 <template>
-  <div class="max-w-md mx-auto bg-white rounded-lg shadow-md p-6">
-    <img
-      :src="products.image"
-      alt="Product Image"
-      class="w-full h-48 object-cover rounded-md mb-4"
-    />
-    <h2 class="text-2xl font-semibold mb-2">{{ products.title }}</h2>
-    <p class="text-lg text-gray-700 mb-2">
-      Price:
-      <span class="font-bold text-green-600">{{
-        products.price | currency
-      }}</span>
-    </p>
-    <p class="italic text-gray-600 mb-4">{{ products.description }}</p>
-    <button
-      class="btn bg-[#12b488] text-white px-4 py-2 rounded-md hover:bg-[#0f8b70] transition duration-200"
-    >
-      Add to Cart
-    </button>
-  </div>
+  <ProductDetail :product="product" />
 </template>
 <script setup>
 const { id } = useRoute().params;
@@ -26,7 +7,7 @@ definePageMeta({
   layout: "products",
 });
 
-const { data: products } = await useFetch(
+const { data: product } = await useFetch(
   `https://fakestoreapi.com/products/${id}`,
   { key: id }
 );
